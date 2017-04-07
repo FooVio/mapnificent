9dca3b12b26042d9810c595ecfb81627f320c3fc
Kutay Kocayurt <kkocayurt@babbel.com>
2017-04-07 17:58:53 +0200

    deploy to github pages

---
+++ mapnificent.js
@@ -0,0 +1,42 @@
+(function() {
+  var map = new Datamap({
+    element: document.getElementById("map"),
+    projection: 'mercator',
+    fills: {
+      defaultFill: "#CCCCCC"
+    }
+  });
+
+  var colors = d3.scale.category20();
+  var languagesWithColor = Object.keys(languages).reduce(function(memo, language, index) {
+    memo[language] = languages[language];
+    memo[language].color = colors(index);
+    return memo;
+  }, {});
+
+  var population;
+  function getCountries(selectedLanguages) {
+    population = 0;
+    map.updateChoropleth({}, { reset: true });
+
+    selectedLanguages.each(function() {
+      fetchCountryInfo(this.value);
+    });
+  }
+
+  function fetchCountryInfo(language) {
+    $.get('https://restcountries.eu/rest/v2/lang/' + language, function(response) {
+      $.each(response, function(index, country) {
+          var opts = {};
+          opts[country.alpha3Code] = languagesWithColor[language].color;
+          population += country.population;
+          map.updateChoropleth(opts);
+      });
+
+      $('#population-count').html(numeral(population).format('0.0a').toUpperCase());
+    });
+  }
+
+  window.getCountries = getCountries;
+  window.languagesWithColor = languagesWithColor;
+})();
